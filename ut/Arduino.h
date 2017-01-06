13f493eb0fb570c21cb02170b8cb039b7f68681c
Krzysztof <Krzysztof.Bartczak@wr.imgtec.org>
2017-01-06 21:04:25 +0100

    Adding Unit tests

---
+++ ut/Arduino.h
@@ -0,0 +1,42 @@
+#ifndef FAKE_ARDUINO_H
+#define FAKE_ARDUINO_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+using namespace std;
+
+class Serial_if
+{
+public:
+    virtual  uint8_t  read() = 0;
+    virtual  uint8_t write(const uint8_t * buff, size_t size ) = 0;
+    virtual  uint8_t available() = 0;
+};
+
+class TSerial
+{
+public:
+    uint8_t  read() { return s->read(); };
+    uint8_t available() { return s->available(); };
+    uint8_t write(const uint8_t * buff, size_t size)
+    {
+       return s->write(buff, size);
+    }
+
+
+    void install(Serial_if * ns)
+    {
+        s = ns;
+    }
+
+private:
+    Serial_if * s;
+} ;
+
+extern TSerial Serial;
+
+
+
+#endif
---
+++ ut/serial_ut.cc
@@ -0,0 +1,45 @@
+#include "gtest/gtest.h" 
+#include "Arduino.h"
+#include "executor.h"
+
+TSerial Serial;
+
+class SerialImpl : public Serial_if
+{
+public:
+    virtual  uint8_t  read()
+    {
+        return 'e';
+    }
+    virtual  uint8_t available()
+    {
+        return 0;
+    }
+    virtual  uint8_t write(const uint8_t * buff, size_t size) 
+    {
+        return (uint8_t)size;
+    }
+
+
+};
+
+void alert (unsigned char c, bool b)
+{
+
+}
+
+
+void Executor::setExecutionTime(uint8_t t, uint16_t s)
+{
+
+}
+
+
+TEST(Serial, t1)
+{
+    SerialImpl myS;
+    Serial.install(&myS);
+
+    ASSERT_TRUE (1);
+}
+
